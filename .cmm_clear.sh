88cff2482eb32c9798dabd1cf317722acab696f0
@raisty <raisty@users.noreply.github.com>
2016-11-04 17:32:38 +0800

    Update and rename .clear.sh to .cmm_clear.sh

---
+++ .cmm_clear.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+read -s -p "Password: " pswd
+echo $pswd | sudo -S sh .cmm_clear_caches.sh
+echo $pswd | sudo -S sh .cmm_clear_logs.sh
+echo $pswd | sudo -S sh .cmm_clear_userlogs.sh
+echo $pswd | sudo -S sh .cmm_clear_thumbs.sh
+echo $pswd | sudo -S sh .cmm_clear_cookies.sh
+echo $pswd | sudo -S sh .cmm_clear_safari.sh
+
+echo $pswd | sudo -S sh .cmm_clear_trash.sh
+echo $pswd | sudo -S sh .cmm_clear_bash.sh
+echo $pswd | sudo -S sh .cmm_arrange_launchpad.sh
